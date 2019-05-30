9e36c565b90cb194d01546ebc6a56dbcebdf6a74
DWITI <paridaranjandwiti@gmail.com>
2019-05-30 09:33:16 +0530

    added Calculatter.java sucessfully

---
+++ CalculatorApplication.java
@@ -0,0 +1,9 @@
+/**
+ * Main Spring	Application.
+ */
+@SpringBootApplication
+public class CalculatorApplication{
+public static void main(String[] args)	{
+										SpringApplication.run(CalculatorApplication.class, args);
+}
+}
